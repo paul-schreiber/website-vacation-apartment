b7b83bc592a8fb413f146489baa5414510f76dd0
Paul Schreiber <schreiber.paul96@gmail.com>
2022-03-17 17:23:48 +0100

    Add primary button

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>{{ caption }}</button>
+</template>
+
+<script>
+export default {
+  props: ['caption']
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: #6bb9a4;
+  border: none;
+  color: white;
+  padding: $margin-small $margin-medium;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: $fs-normal;
+  border-radius: 15px;
+  cursor: pointer;
+}
+</style>
