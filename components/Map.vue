814e75b9d6a9905eb9587d064b8b3b88bf2d3fbf
Paul Schreiber <schreiber.paul96@gmail.com>
2022-03-22 15:36:21 +0100

    Add map basics

---
+++ components/Map.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    let mapKitScript = document.createElement("script");
+    mapKitScript.setAttribute(
+      "src",
+      "https://cdn.apple-mapkit.com/mk/5.x.x/mapkit.js"
+    );
+    document.head.appendChild(mapKitScript);
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+#map {
+  height: 300px;
+  margin: $margin-large auto;
+  background-color: lightgray;
+  border-radius: 20px;
+  box-shadow: $soft-shadow;
+}
+</style>
