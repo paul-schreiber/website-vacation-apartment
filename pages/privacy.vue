e0b1e043571e07fe47b7db5c821e06cc286da433
Paul Schreiber <schreiber.paul96@gmail.com>
2022-07-07 16:49:28 +0200

    Add template for subpages

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,3 +1,44 @@
 <template>
-    <div>Datenschutz</div>
+  <div>
+    <Navbar />
+    Datenschutzerklärung
+    <Footer
+      :name="content.name"
+      :address="content.address"
+      :telephone="content.telephone"
+      :mail="content.mail"
+    />
+  </div>
 </template>
+
+<script>
+import content from "../content";
+export default {
+  data() {
+    return {
+      content: content,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+
+html {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
+  -moz-box-sizing: border-box; /* Firefox, other Gecko */
+  box-sizing: border-box; /* Opera/IE 8+ */
+  color: $primary-font-color;
+  scroll-behavior: smooth;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+</style>
